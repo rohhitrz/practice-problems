10986f4fe73a0706ff58c3d367da87b0244395b7
Rohit <rohhit.rz@gmail.com>
2024-02-27 12:05:01 +0530

    First Commit

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo {
+
+    static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int res = sum(200,400);
+        System.out.println(res);
+    }
+}
